7e9921859284ee0d9a35b105fac9652fd2a89edb
hanxu1997 <807174205@qq.com>
2018-03-19 23:42:06 +0800

    basic

--- CGhw2/CG_hw2/HelloTriangle/main.cpp
+++ CGhw2/CG_hw2/HelloTriangle/main.cpp
@@ -1,4 +1,3 @@
-
 #include"Triangle.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -10,7 +9,24 @@
 #include <iostream>
 using namespace std;
 
-
+// 顶点着色器源码
+const char *vertexShaderSource = "#version 440 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   ourColor = aColor;\n"
+"}\0";
+// 片段着色器源码
+const char *fragmentShaderSource = "#version 440 core\n"
+"out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(ourColor, 1.0f);\n"
+"}\n\0";
 int main()
 {
 	// 初始化GLFW
@@ -31,9 +47,9 @@ int main()
 	}
 	// 通知GLFW将我们窗口的上下文设置为当前线程的主上下文
 	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 	// 注册这个函数，告诉GLFW我们希望每当窗口调整大小的时候调用这个函数
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
 	// GLAD: 管理的OpenGL函数指针
 	// 调用任何OpenGL函数之前需要初始化GLAD
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -45,67 +61,127 @@ int main()
 	// Setup ImGui binding
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
 	ImGui_ImplGlfwGL3_Init(window, true);
 
 	// Setup style
 	ImGui::StyleColorsDark();
-	//ImGui::StyleColorsClassic();
 	bool show_demo_window = true;
-	bool show_another_window = true;
-	ImVec4 clear_color = ImVec4(0.30f, 0.40f, 0.50f, 1.00f);
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.92f, 0.92f, 0.92f, 1.0f);
 
+	// 创建并编译着色器程序
+	// 1. 顶点着色器
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// 检查顶点着色器是否编译成功
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
+	}
+	// 2. 片段着色器
+	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// 检查片段着色器是否编译成功
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << endl;
+	}
+	// 3. 链接着色器
+	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// 检查是否链接成功
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << endl;
+	}
+	// 链接完成后删除着色器对象
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
-	//-----------------------
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	float vertices[] = {
+		// positions         // colors
+		-0.5f, -0.2f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom left
+		0.0f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,    // top 
+		0.5f, -0.2f, 0.0f, 0.0f, 0.0f, 1.0f  // bottom right
+	};
+	// 顶点缓冲对象，顶点数组对象
+	unsigned int VBO;
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	// 绑定顶点数组对象
+	glBindVertexArray(VAO);
+	// 把我们的顶点数组复制到一个顶点缓冲中，供OpenGL使用
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 链接顶点属性
+	// 位置属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// 颜色属性
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	// 激活着色器程序对象
+	glUseProgram(shaderProgram);
+
+	float left_color[] = { 1.0, 0.0, 0.0 };
+	float top_color[] = { 0.0, 1.0, 0.0 };
+	float right_color[] = { 0.0, 0.0, 1.0 };
+
+	//-------------------------------------------------------------------------------------
 	// 渲染循环 render loop
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
 		ImGui_ImplGlfwGL3_NewFrame();
-		// 1. Show a simple window.
-		// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
-		{
-			static float f = 0.0f;
-			static int counter = 0;
-			ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
-			ImGui::Checkbox("Another Window", &show_another_window);
-
-			if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-				counter++;
-			ImGui::SameLine();
-			ImGui::Text("counter = %d", counter);
-
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		}
-
-		// 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
-		if (show_another_window)
-		{
-			ImGui::Begin("Another Window", &show_another_window);
-			ImGui::Text("Hello from another window!");
-			if (ImGui::Button("Close Me"))
-				show_another_window = false;
-			ImGui::End();
-		}
+		glClearColor(clear_color.x, clear_color.y, clear_color.z, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
 
 
-		// 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
-		if (show_demo_window)
-		{
-			ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
-			ImGui::ShowDemoWindow(&show_demo_window);
-		}
+		// 把我们的顶点数组复制到一个顶点缓冲中，供OpenGL使用
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		// 颜色属性
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
+		// 激活着色器程序对象
+		glUseProgram(shaderProgram);
+		// 渲染三角形
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		ImGui::Begin("menu", &show_demo_window);
+		// ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+		ImGui::ColorEdit3("leftPoint", (float*)&left_color);
+		ImGui::ColorEdit3("TopPoint", (float*)&top_color);
+		ImGui::ColorEdit3("RightPoint", (float*)&right_color);
+		vertices[3] = left_color[0];
+		vertices[4] = left_color[1];
+		vertices[5] = left_color[2];
+		vertices[9] = top_color[0];
+		vertices[10] = top_color[1];
+		vertices[11] = top_color[2];
+		vertices[15] = right_color[0];
+		vertices[16] = right_color[1];
+		vertices[17] = right_color[2];
+		ImGui::End();
+
 		// Rendering
 		int display_w, display_h;
 		glfwGetFramebufferSize(window, &display_w, &display_h);
 		glViewport(0, 0, display_w, display_h);
-		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-		glClear(GL_COLOR_BUFFER_BIT);
 		ImGui::Render();
 		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 		glfwSwapBuffers(window);
@@ -114,6 +190,8 @@ int main()
 	// 释放/删除之前分配的所有资源
 	ImGui_ImplGlfwGL3_Shutdown();
 	ImGui::DestroyContext();
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 	glfwTerminate();
 	return 0;
 }
